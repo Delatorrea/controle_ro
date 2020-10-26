c556bd310e73b06559e4444fa1f7eae1493840bf
Emerson Delatorre <emerson@delatorre.dev>
2020-10-25 21:51:03 -0300

    Pequenas correções

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Modelo.RegistroDeOcorrencia import RegistroDeOcorrencia
 
 
-r = RegistroDeOcorrencia(r'C:\tmp\PythonWord\RO_PETROBRAS.docx')
+r = RegistroDeOcorrencia(r'C:\tmp\PythonWord\RO.docx')
 r.carrega_word()
 print(r)
