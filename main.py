d937021f4e0bc9875edcbc54fab8073303d62b5e
Emerson Delatorre de Andrade (VIX Macaé) <emersona@vix.com.br>
2021-02-19 15:33:40 -0300

    Inserida referências e modelo de banco de dados.

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ from Modelo.RegistroDeOcorrencia import RegistroDeOcorrencia
 
 
 def inserir():
-    w = RegistroDeOcorrenciaWord('/home/emerson/PycharmProjects/controle_ro/RO.docx')
+    w = RegistroDeOcorrenciaWord('./RO.docx')
     w.carrega_arquivo()
 
-    c = ServicoContrato.buscar_por_instrumento_contratual(w.instrumento_contratual)
+    c = ServicoContrato.buscar_por_instrumento_contratual(
+        w.instrumento_contratual
+    )
 
     if c is None:
-        c = Contrato(w.instrumento_contratual, w.contrato_sap, w.orgao, w.contratada, w.autorizacao_servico,
-                     w.prazo_contratual, w.obj_contrato, w.inicio, w.termino, w.local_execucao)
+        c = Contrato(
+            w.instrumento_contratual, w.contrato_sap, w.orgao, w.contratada,
+            w.autorizacao_servico, w.prazo_contratual, w.obj_contrato,
+            w.inicio, w.termino, w.local_execucao
+        )
 
         ServicoContrato.inserir(c)
 
     if ServicoRegistroDeOcorrencia.buscar_por_numero(w.numero) is None:
-        r = RegistroDeOcorrencia(w.numero, w.tipo.value, w.data, w.corpo_fiscalizacao, w.corpo_contratada,
-                                 w.assinatura_fiscalizacao, w.corpo_contratada, c)
+        r = RegistroDeOcorrencia(
+            w.numero, w.tipo.value, w.data, w.corpo_fiscalizacao,
+            w.corpo_contratada, w.assinatura_fiscalizacao,
+            w.corpo_contratada, c)
 
         ServicoRegistroDeOcorrencia.inserir(r)
     else:
